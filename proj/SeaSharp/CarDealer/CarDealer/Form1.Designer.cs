2ef55c668087dbd69c3d77b699fe165e7cc6532c
Sebs <wiewiora.seb@gmail.com>
2017-11-26 21:15:45 +0100

    car dealer init

---
+++ proj/SeaSharp/CarDealer/CarDealer/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace CarDealer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.carBrandComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.engineComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.carModelComboBox = new System.Windows.Forms.ComboBox();
+            this.isMetallicLacquerCheckBox = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lacquerColorComboBox = new System.Windows.Forms.ComboBox();
+            this.additionalOptionsCheckedListBox = new System.Windows.Forms.CheckedListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Marka samochodu";
+            // 
+            // carBrandComboBox
+            // 
+            this.carBrandComboBox.FormattingEnabled = true;
+            this.carBrandComboBox.Location = new System.Drawing.Point(28, 45);
+            this.carBrandComboBox.Name = "carBrandComboBox";
+            this.carBrandComboBox.Size = new System.Drawing.Size(121, 21);
+            this.carBrandComboBox.TabIndex = 3;
+            this.carBrandComboBox.Text = "wybierz markę";
+            this.carBrandComboBox.SelectedIndexChanged += new System.EventHandler(this.carBrandComboBox_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(285, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Silnik";
+            // 
+            // engineComboBox
+            // 
+            this.engineComboBox.FormattingEnabled = true;
+            this.engineComboBox.Location = new System.Drawing.Point(285, 44);
+            this.engineComboBox.Name = "engineComboBox";
+            this.engineComboBox.Size = new System.Drawing.Size(121, 21);
+            this.engineComboBox.TabIndex = 8;
+            this.engineComboBox.Text = "wybierz silnik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Model";
+            // 
+            // carModelComboBox
+            // 
+            this.carModelComboBox.FormattingEnabled = true;
+            this.carModelComboBox.Location = new System.Drawing.Point(158, 44);
+            this.carModelComboBox.Name = "carModelComboBox";
+            this.carModelComboBox.Size = new System.Drawing.Size(121, 21);
+            this.carModelComboBox.TabIndex = 6;
+            this.carModelComboBox.Text = "wybierz model";
+            // 
+            // isMetallicLacquerCheckBox
+            // 
+            this.isMetallicLacquerCheckBox.AutoSize = true;
+            this.isMetallicLacquerCheckBox.Location = new System.Drawing.Point(412, 44);
+            this.isMetallicLacquerCheckBox.Name = "isMetallicLacquerCheckBox";
+            this.isMetallicLacquerCheckBox.Size = new System.Drawing.Size(91, 17);
+            this.isMetallicLacquerCheckBox.TabIndex = 10;
+            this.isMetallicLacquerCheckBox.Text = "Lakier metalik";
+            this.isMetallicLacquerCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(509, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Kolor lakieru";
+            // 
+            // lacquerColorComboBox
+            // 
+            this.lacquerColorComboBox.FormattingEnabled = true;
+            this.lacquerColorComboBox.Location = new System.Drawing.Point(509, 44);
+            this.lacquerColorComboBox.Name = "lacquerColorComboBox";
+            this.lacquerColorComboBox.Size = new System.Drawing.Size(121, 21);
+            this.lacquerColorComboBox.TabIndex = 11;
+            this.lacquerColorComboBox.Text = "wybierz kolor";
+            // 
+            // CheckedListBox1
+            // 
+            this.additionalOptionsCheckedListBox.FormattingEnabled = true;
+            this.additionalOptionsCheckedListBox.Items.AddRange(new object[] {
+            "Automatyczna skrzynia biegów",
+            "Klimatyzacja",
+            "Wspomaganie"});
+            this.additionalOptionsCheckedListBox.Location = new System.Drawing.Point(28, 94);
+            this.additionalOptionsCheckedListBox.Name = "CheckedListBox1";
+            this.additionalOptionsCheckedListBox.Size = new System.Drawing.Size(132, 49);
+            this.additionalOptionsCheckedListBox.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 78);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Opcje dodatkowe:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 323);
+            this.Controls.Add(this.additionalOptionsCheckedListBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lacquerColorComboBox);
+            this.Controls.Add(this.isMetallicLacquerCheckBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.engineComboBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.carModelComboBox);
+            this.Controls.Add(this.carBrandComboBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.ComboBox carBrandComboBox;
+        internal System.Windows.Forms.Label label3;
+        internal System.Windows.Forms.ComboBox engineComboBox;
+        internal System.Windows.Forms.Label label2;
+        internal System.Windows.Forms.ComboBox carModelComboBox;
+        internal System.Windows.Forms.CheckBox isMetallicLacquerCheckBox;
+        internal System.Windows.Forms.Label label4;
+        internal System.Windows.Forms.ComboBox lacquerColorComboBox;
+        internal System.Windows.Forms.CheckedListBox additionalOptionsCheckedListBox;
+        internal System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ proj/SeaSharp/CarDealer/CarDealer/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarDealer
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<String, List<String>> Cars = new Dictionary<String, List<String>>();
+        List<String> carBrands = new List<String>();
+        List<String> carModels = new List<String>();
+        List<String> engines = new List<String>();
+
+        String[] initialCarBrands = { "Opel", "BMW", "Mitsubishi", "Citroen", "Toyota" };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            carBrands.AddRange(initialCarBrands);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            carBrandComboBox.DataSource = carBrands;
+
+        }
+
+        private void carBrandComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+
+    }
+}
