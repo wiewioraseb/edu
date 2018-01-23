04ba83e718ccfe72b9e00d550c2cd8babb5f74e6
Sebastian Wiewiora <swiewiora@cctechnology.pl>
2018-01-23 20:42:43 +0100

    2-6 ex

--- proj/Cpp/exercises/StandardExercises/StandardExercises/ex2-6.cpp
+++ proj/Cpp/exercises/StandardExercises/StandardExercises/ex2-6.cpp
@@ -4,20 +4,38 @@
 using namespace std;
 
 
-struct Wiz {
+struct Wiz_tab {
 public:
-	string pierwszasStringProperty;
-	int drugaIntProperty;
-	char trzebaIntTabProperty[10];
+	char imie[10];
 };
+
+const int TABLE_SIZE = 3;
 
 void ex2_6() {
 	cout << "START zadanie2-6" << endl;
 
-	struct Wiz * wiz = new struct Wiz;
+	struct Wiz_tab **wiz = new struct Wiz_tab *[3];
 
+	cout << "Wczytuje imie:" << endl;
+	for (int i = 0; i < 3; i++) {
+		cout << endl;
+		
+		//wiz[i] = new struct Wiz_tab;
+		*(wiz+i) = new struct Wiz_tab;
 
+		//cin >> (*wiz[i]).imie;
+		//cin >> wiz[i]->imie;
+		cin >> (*(*(wiz + i))).imie;
+	}
+
+	for (int i = 0; i < 3; i++) {
+		//cout << "wiz[" << i << "]->imie= " << wiz[i]->imie <<endl;
+		//cout << "wiz[" << i << "]->imie= " << (*(*(wiz + i))).imie << endl;
+		cout << "wartosc x= " << (*(*(wiz + i))).imie << endl;
+	}
+
+	delete[] wiz;
+	wiz = NULL;
 
 	cout << endl << "KONIEC" << endl;
-
 }
