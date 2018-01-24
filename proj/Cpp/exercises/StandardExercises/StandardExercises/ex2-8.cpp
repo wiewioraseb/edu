e0dd3c6796e091e7afc5c360b7c2a442e40a9db8
Sebastian W <wiewiora.seb@gmail.com>
2018-01-24 21:49:09 +0100

    partial 2-8 ex

--- proj/Cpp/exercises/StandardExercises/StandardExercises/ex2-8.cpp
+++ proj/Cpp/exercises/StandardExercises/StandardExercises/ex2-8.cpp
@@ -7,6 +7,43 @@ using namespace std;
 
 void ex2_8() {
 	cout << "START zadanie2-8" << endl;
+	int trescDlugosc = 5;
+	char **tresc = new char*[trescDlugosc];
+
+	cout << "Podaj tresc:" << endl;
+	for (int i = 0; i < trescDlugosc; i++) {
+		tresc[i] = new char;
+		cin >> tresc[i];
+	}
+	cout << "Wypisz tresc:" << endl;
+	for (int i = 0; i < trescDlugosc; i++) {
+		cout << tresc[i];
+	}
+	cout << endl;
+
+	char *szukanyWskaznik = wskazdnikDoDrugiegoC(*tresc, trescDlugosc);
+	cout << endl << "Szukany wsaznik adres:" << szukanyWskaznik << endl;
+	cout << endl << "Szukany wsaznik wartosc:" << *szukanyWskaznik << endl;
+
 
 	cout << endl << "KONIEC" << endl;
+}
+
+char * wskazdnikDoDrugiegoC(char lancuch[], int dlugosc) {
+	cout << endl << "Wejsc do funkcji." << endl;
+	int licznik = 0;
+	for (int i = 0; i < dlugosc; i++) {
+		cout << endl << "i:" << i  << " value: " << lancuch[i] << endl;
+		if ( lancuch[i] == 'c') {
+			cout << endl << "Znaleziono!" << endl;
+			licznik++;
+		}
+		if (licznik == 2) {
+			cout << endl << "Znaleziono 'c', adres:" << &lancuch[i] << endl;
+			cout << endl << "Znaleziono 'c', wartosc:" << lancuch[i] << endl;
+
+			return &lancuch[i];
+		}
+	}
+	return NULL;
 }
--- proj/Cpp/exercises/StandardExercises/StandardExercises/ex2-8.h
+++ proj/Cpp/exercises/StandardExercises/StandardExercises/ex2-8.h
@@ -1,2 +1,3 @@
 #pragma once
 void ex2_8(void);
+char * wskazdnikDoDrugiegoC(char[] , int );
