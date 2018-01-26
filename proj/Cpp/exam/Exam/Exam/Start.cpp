f28ac87b785f9e4524cfc8b84da4f39574b80fd4
Sebastian W <wiewiora.seb@gmail.com>
2018-01-26 22:04:01 +0100

    init for exam

---
+++ proj/Cpp/exam/Exam/Exam/Start.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "zadanie1.h"
+
+using namespace std;
+
+int main(void) {
+	cout << "Podaj nr zadania: ";
+	int numerZadania = 1;
+	cout << endl << "Wybrano zadanie: " << numerZadania << endl;
+
+	switch (numerZadania) {
+	case 1: {
+		zadanie1();
+		break;
+	}
+	//case 2: {
+	//	ex2_9();
+	//	break;
+	//}
+}
+
+	system("pause");
+
+	return 0;
+}
---
+++ proj/Cpp/exam/Exam/Exam/zadanie1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+using namespace std;
+
+void zadanie1() {
+	cout << "START zadanie1" << endl;
+
+	cout << endl << "KONIEC" << endl;
+
+}
