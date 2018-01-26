64c6acf52d1d289629e3124a1a05593f9ad4b5f9
Sebastian W <wiewiora.seb@gmail.com>
2018-01-26 17:13:24 +0100

    init 3-1 simple implementation

--- proj/Cpp/exercises/StandardExercises/StandardExercises/ex3-1.cpp
+++ proj/Cpp/exercises/StandardExercises/StandardExercises/ex3-1.cpp
@@ -8,6 +8,25 @@ using namespace std;
 void ex3_1() {
 	cout << "START zadanie3-1" << endl;
 
+	Pies *burek = new Pies();
+	burek->Spaceruj(45);
+	Pies szczurek("Szczurek", 37);
+	szczurek.Spaceruj(22);
+
 	cout << endl << "KONIEC" << endl;
 }
 
+Pies::Pies(string i, int e) {
+	imie = i;
+	energia = e;
+}
+Pies::~Pies() {
+	cout << endl << "DESTRUKTOR";
+}
+
+void Pies::Spaceruj(int czas) {
+	cout << endl << "Pies " << imie 
+		<< " spaceruje przez " << czas << " minut." 
+		<< " Ma " << energia << " energii."
+		<< endl;
+}
--- proj/Cpp/exercises/StandardExercises/StandardExercises/ex3-1.h
+++ proj/Cpp/exercises/StandardExercises/StandardExercises/ex3-1.h
@@ -1,2 +1,19 @@
 #pragma once
+#include <string>
 void ex3_1();
+
+class Pies 
+{
+public:
+	Pies() {
+		imie = "Burek";
+		energia = 30;
+	}
+	Pies(std::string imie, int energia);
+	Pies(Pies&);
+	~Pies();
+	void Spaceruj(int);
+private:
+	std::string imie;
+	int energia;
+};
