05dc034d94ea664af3b063e2d6ea1e8704e99cc5
Sebastian W <wiewiora.seb@gmail.com>
2018-01-26 21:05:49 +0100

    init inheritance

--- proj/Cpp/exercises/StandardExercises/StandardExercises/ex3-3.cpp
+++ proj/Cpp/exercises/StandardExercises/StandardExercises/ex3-3.cpp
@@ -9,18 +9,6 @@ void ex3_3() {
 	cout << "START zadanie3-3" << endl;
 
 
+
 	cout << endl << "KONIEC" << endl;
-}
-
-Mysz::Mysz(string i, int e) {
-	imie = i;
-	energia = e;
-}
-
-Mysz::Mysz(Mysz& k) {
-	cout << endl << "Konstruktor kopiuj¹cy." << endl;
-}
-
-Mysz::~Mysz() {
-	cout << endl << "DESTRUKTOR" << endl;
 }
--- proj/Cpp/exercises/StandardExercises/StandardExercises/ex3-3.h
+++ proj/Cpp/exercises/StandardExercises/StandardExercises/ex3-3.h
@@ -2,17 +2,87 @@
 #include <string>
 void ex3_3();
 
-class Mysz
+class Gryzon
+{
+public:
+	Gryzon() {
+		imie = "Jakis_gryzon";
+		energia = 88;
+	}
+	Gryzon(std::string i, int e) {
+		imie = i;
+		energia = e;
+	}
+	Gryzon(Gryzon& k) {
+		cout << "KONSTRUTOR kopiujacy" << endl;
+	}
+	virtual ~Gryzon() {
+		cout << "DESTRUKTOR Gryzon" << endl;
+	}
+	void sound() {
+		cout << "Sound Gryzon" << endl;
+	}
+
+private:
+	std::string imie;
+	int energia;
+	std::string typ = "gryzon";
+
+};
+
+class Mysz : public Gryzon
 {
 public:
 	Mysz() {
 		imie = "Mickey";
 		energia = 88;
 	}
-	Mysz(std::string i, int e);
-	Mysz(Mysz& k);
-	~Mysz();
+	Mysz(std::string i, int e) {
+		imie = i;
+		energia = e;
+	}
+	Mysz(Mysz& k) {
+		cout << "KONSTRUTOR kopiujacy" << endl;
+	}
+	~Mysz() {
+		cout << "DESTRUKTOR mysz" << endl;
+	}
+	void sound() {
+		cout << "Sound Mysz" << endl;
+	}
+
 private:
 	std::string imie;
 	int energia;
+	std::string typ = "mysz";
+
+};
+
+
+class Szczur : public Gryzon
+{
+public:
+	Szczur() {
+		imie = "Szczurek";
+		energia = 88;
+	}
+	Szczur(std::string i, int e) {
+		imie = i;
+		energia = e;
+	}
+	Szczur(Szczur& k) {
+		cout << "KONSTRUTOR kopiujacy" << endl;
+	}
+	~Szczur() {
+		cout << "DESTRUKTOR Szczur" << endl;
+	}
+	void sound() {
+		cout << "Sound Szczur" << endl;
+	}
+
+private:
+	std::string imie;
+	int energia;
+	std::string typ = "szczur";
+
 };
