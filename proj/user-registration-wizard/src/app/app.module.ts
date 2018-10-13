dc8d6fb1caa56364e98a83b529e3ad035729c595
wiewioraseb <wiewiora.seb@gmail.com>
2018-10-13 21:08:50 +0200

    add top bar component

--- proj/user-registration-wizard/src/app/app.module.ts
+++ proj/user-registration-wizard/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopBarModule } from './top-bar/top-bar.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TopBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- proj/user-registration-wizard/src/app/top-bar/top-bar.module.ts
+++ proj/user-registration-wizard/src/app/top-bar/top-bar.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopBarComponent } from './top-bar.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [TopBarComponent]
 })
 export class TopBarModule { }
