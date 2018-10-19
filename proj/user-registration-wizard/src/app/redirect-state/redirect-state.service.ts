9735115844ce85bb869a922845e7fa5bb9ebe431
Sebastian Wiewiora <swiewiora@cctechnology.pl>
2018-10-19 16:22:23 +0200

    create state checker map for navigation guard

--- proj/user-registration-wizard/src/app/redirect-state/redirect-state.service.ts
+++ proj/user-registration-wizard/src/app/redirect-state/redirect-state.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
 
+type ComponentName = 'login-detail' | 'select-group' | 'add-avatar' | 'form-submit';
+
 @Injectable({
   providedIn: 'root'
 })
 export class RedirectStateService {
+  private stateMap = new Map<string, boolean>([
+    ['login-detail', false],
+    ['select-group', false],
+    ['add-avatar', false],
+    ['form-submit', false]
+  ]);
 
   constructor() { }
 
-  getState(): boolean {
-    return true;
+  canNavigate(componentName: ComponentName): boolean {
+    return this.stateMap.get(componentName);
+  }
+
+  getAll(): Map<string, boolean> {
+    return this.stateMap;
+  }
+
+  setNavigateState(componentName: ComponentName, state: boolean): void {
+    this.stateMap.set(componentName, state);
   }
 }
